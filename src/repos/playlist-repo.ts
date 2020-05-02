f7757629152ee42076c42ad39d031ee5d3a94073
Jose Angeles <humblekgames@gmail.com>
2020-05-02 17:29:28 -0400

    Added All repos for each model, also added a crud-repo.ts

--- src/repos/playlist-repo.ts
+++ src/repos/playlist-repo.ts
@@ -1,4 +1,5 @@
-import {Artist} from '../models/artist';
-import {Album} from '../models/album';
-import {Track} from '../models/track';
 import {Playlist} from  '../models/playlist';
+
+export class PlaylistRepository {
+    async get
+}
