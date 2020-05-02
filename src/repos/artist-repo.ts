b31f488d7cf1060efeae19d7ebb63b2dfd9ffdd4
Jose Angeles <humblekgames@gmail.com>
2020-05-02 19:27:22 -0400

    setting up the artist-repo and the connection with the DB

--- src/repos/artist-repo.ts
+++ src/repos/artist-repo.ts
@@ -1,1 +1,28 @@
 import {Artist} from '../models/artist';
+import {CrudRepository} from './crud-repo';
+import {PoolClient} from 'pg';
+import {connectionPool} from '..';
+import {mapArtistResultSet} from '../util/artist-set-mapper';
+
+
+export class ArtistRepository implements CrudRepository<Artist>{
+    async getAll(): Promise<Artist[]>{
+
+    };
+    async getNameById(id: number): Promise<Artist> {
+
+    };
+
+    async addNew(newArtist: Artist): Promise<boolean> {
+
+    };
+
+    async update(artist: Artist): Promise<boolean> {
+
+    };
+
+    async deleteById(id: number): Promise<boolean> {
+
+    }
+
+}
--- src/repos/crud-repo.ts
+++ src/repos/crud-repo.ts
@@ -1,6 +1,6 @@
 export interface CrudRepository<T> {
     getAll(): Promise<T[]>;
-    getNameById(id: number): string;
+    getNameById(id: number): Promise<T>;
     addNew(newObj: T): Promise<boolean>;
     update(object: T): Promise<boolean>;
     deleteById(id: number): Promise<boolean>;
