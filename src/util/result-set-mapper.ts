97bfb120e2e9a1611d0c38c400de0ca0525edcbb
Jose Angeles <humblekgames@gmail.com>
2020-05-03 17:20:05 -0400

    Updated result-set-mapper with all schemas made

--- src/util/result-set-mapper.ts
+++ src/util/result-set-mapper.ts
@@ -1,5 +1,8 @@
-import {ArtistSchema} from './schemas';
-import {Artist} from '../models/artist';
+import { ArtistSchema, AlbumSchema, SongSchema, PlaylistSchema } from './schemas';
+import { Artist } from '../models/artist';
+import { Album } from '../models/album';
+import { Song } from '../models/song';
+import { Playlist } from '../models/playlist';
 
 export function mapArtistResultSet(resultSet: ArtistSchema): Artist {
     if (!resultSet) {
@@ -11,3 +14,38 @@ export function mapArtistResultSet(resultSet: ArtistSchema): Artist {
         resultSet.artist_name
     );   
 };
+
+export function mapAlbumResultSet(resultSet: AlbumSchema): Album {
+    if(!resultSet) {
+        return {} as Album;
+    };
+
+    return new Album (
+        resultSet.album_id,
+        resultSet.album_name,
+        resultSet.year_released
+    );
+};
+
+export function mapSongResultSet(resultSet: SongSchema): Song {
+    if(!resultSet) {
+        return {} as Song;
+    };
+
+    return new Song (
+        resultSet.song_id,
+        resultSet.song_name,
+    );
+};
+
+export function mapPlaylistResultSet(resultSet: PlaylistSchema): Playlist {
+    if(!resultSet) {
+        return {} as Playlist;
+    };
+
+    return new Playlist (
+        resultSet.playlist_id,
+        resultSet.playlist_name,
+        resultSet.playlist_desc
+    );
+}
