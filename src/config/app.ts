52a6195af22f34da3d60ca3fba7d279fd0516214
Jose Angeles <humblekgames@gmail.com>
2020-05-02 22:49:19 -0400

    Fixed typos in artist config in app.ts

--- src/config/app.ts
+++ src/config/app.ts
@@ -2,7 +2,7 @@ import {ArtistRepository} from '../repos/artist-repo';
 import {ArtistService} from '../services/artist-service';
 
 const artistRepo = new ArtistRepository();
-const artistService = new ArtistRepository();
+const artistService = new ArtistService(artistRepo);
 
 
 export default {
